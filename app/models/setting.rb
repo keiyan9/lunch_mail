725e80e867c1ccb59e0571c7145ceaaed40c8349
interu <beautybeast07@gmail.com>
2011-10-07 18:41:40 +0900

    change default settings

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,2 +1,5 @@
 class Setting < ActiveRecord::Base
+
+  validates_presence_of :area
+
 end
--- config/application.rb
+++ config/application.rb
@@ -28,10 +28,10 @@ module Casamotta
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = 'ja'
+    config.i18n.default_locale = :ja
 
     # JavaScript files you want as :defaults (application.js is always included).
-    config.action_view.javascript_expansions[:defaults] = %w(jquery jquery-ui jquery-ujs)
+    config.action_view.javascript_expansions[:defaults] = %w(jquery jquery-ui jquery_ujs)
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
