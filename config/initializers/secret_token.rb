e6aa1b5b9521dbc1ece532ef6dd8f7c08a2d725b
interu <beautybeast07@gmail.com>
2011-10-07 16:25:23 +0900

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Casamotta::Application.config.secret_token = '5ae6c191ec6095190f77df2921e6f92496910b493d4685fdfcfb607f32d94e723abcbda1f041374744597de0c4864fd4f8a8f71baaac8fd6c18ebcafd3fda6ec'
