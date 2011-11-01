4057d7afe56d95053f5a25d40bf0ffc6d4d5a24c
keiyan9 <nanbakouichi@nanba-no-MacBook-Air.local>
2011-11-01 12:01:19 +0900

    Change np page, Add mail to group

--- app/controllers/notice_points_controller.rb
+++ app/controllers/notice_points_controller.rb
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 class NoticePointsController < ApplicationController
 
+  def index
+    @setting = current_user.setting
+  end
+
   def new
     @setting = current_user.setting
     @notice_point = NoticePoint.new
@@ -11,6 +15,16 @@ class NoticePointsController < ApplicationController
     @notice_point = NoticePoint.new(params[:notice_point])
     @notice_point.user_id = current_user.id
     @notice_point.save
-    redirect_to setting_path(@setting), :notice => '通知先が追加されました。'
+    redirect_to setting_notice_points_path(@setting), :notice => '通知先が追加されました。'
+  end
+
+  def destroy
+    @setting = current_user.setting
+    @notice_point = current_user.notice_points.find(params[:id])
+    @notice_point.destroy
+
+    respond_to do |format|
+      format.html { redirect_to setting_notice_points_path(@setting)}
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,12 +15,16 @@ class User < ActiveRecord::Base
     self.select_target_users.each do |user|
       response_geo = ApiAccess.geo_api_get({:q => user.setting.area})
       categories = [100, 200, 300, 400]
-      response_category = "CTG#{categories[rand(categories.length)]}"
-      response_gnavi = ApiAccess.gnavi_api_get({:keyid => "dd0f3c4c27d1f6b371cd99acbebe97fb", :latitude => response_geo.result.coordinate.lat, :longitude => response_geo.result.coordinate.lng, :range => 1, :hit_per_page => 999, :category_l => response_category})
-      user.notice_points.each do |np|
-        Notifier.notice_email(np,response_gnavi)
-        Notifier.deliver_notice_email(np,response_gnavi)
-        logger.info "[Mail] send email to #{np.email}"
+      np_groups = []
+      user.notice_points.sort_by{ rand }.each_slice(4){ |user_group| np_groups << user_group }
+      np_groups.each do |np_group|
+        response_category = "CTG#{categories[rand(categories.length)]}"
+        response_gnavi = ApiAccess.gnavi_api_get({:keyid => "dd0f3c4c27d1f6b371cd99acbebe97fb", :latitude => response_geo.result.coordinate.lat, :longitude => response_geo.result.coordinate.lng, :range => 1, :hit_per_page => 999, :category_l => response_category})
+        np_group.each do |np|
+          Notifier.notice_email(np,response_gnavi)
+          Notifier.deliver_notice_email(np,response_gnavi)
+          logger.info "[Mail] send email to #{np.email}"
+        end
       end
     end
   end
