264b71c97dcd514784a0d0dd4fa797780521c412
keiyan9 <nanbakouichi@nanba-no-MacBook-Air.local>
2011-11-26 20:24:45 +0900

    夜間営業店舗と休業店舗の削除、緯度・軽度の測地系タイプ変更

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -2,9 +2,13 @@
 class Notifier < ActionMailer::Base
   default :from => "ランチメール <support@lunchmail.jp>"
 
-  def notice_email(user,shop,members)
-    subject = "☆本日のオススメ店＆ランチメンバー☆"
-    body =  <<"LUNCHMAIL"
+  def notice_email(user,shop=nil,members=nil)
+    if shop == nil
+      subject = "ご報告"
+      body = "本日ランチ営業している店舗は、付近で見つかりませんでした"
+    else
+      subject = "☆本日のオススメ店＆ランチメンバー☆"
+      body =  <<"LUNCHMAIL"
 ＜本日のオススメ店＞
 #{shop.name}
 
@@ -14,6 +18,7 @@ class Notifier < ActionMailer::Base
 ＜店舗URL＞
 #{shop.url_mobile}
 LUNCHMAIL
+    end
 
     mail(:to => user.email, :subject => subject, :body => body)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 class User < ActiveRecord::Base
   has_one :setting
   has_many :notice_points
@@ -10,39 +11,53 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
 
+  WDAYS = ["日曜日", "月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日"].freeze
 
   def self.send_notifications
     self.select_target_users.each do |user|
       response_geo = ApiAccess.geo_api_get({:q => user.setting.area})
-      categories = [100, 200, 300, 400]
-      response_category = "CTG#{categories[rand(categories.length)]}"
-      response_gnavi = ApiAccess.gnavi_api_get({:keyid => "dd0f3c4c27d1f6b371cd99acbebe97fb", :latitude => response_geo.result.coordinate.lat, :longitude => response_geo.result.coordinate.lng, :range => 1, :hit_per_page => 999, :category_l => response_category})
-      if response_gnavi.response.rest.instance_of?(Array)
-        shop_count = response_gnavi.response.rest.size
-        shop = response_gnavi.response.rest[rand(shop_count)]
+      response_gnavi = ApiAccess.gnavi_api_get({:keyid => "dd0f3c4c27d1f6b371cd99acbebe97fb", :latitude => response_geo.result.coordinate.lat, :longitude => response_geo.result.coordinate.lng, :range => 1, :hit_per_page => 999, :input_coordinates_mode => 2})
+
+      day = Time.now
+      response_gnavi.response.rest.delete_if{ |rest| rest.holiday == WDAYS[day.wday] }
+
+      rests = response_gnavi.response.rest.select{ |rest| rest.opentime.include?("11:00") || rest.opentime.include?("11:25") || rest.opentime.include?("11:30") || rest.opentime.include?("12:00")}
+
+      if rests.empty?
+        np = user.notice_points
+        Notifier.notice_email(np)
+        Notifier.deliver_notice_email(np)
+        logger.info "[Mail] send email to #{np.email}"
       else
-        shop = response_gnavi.response.rest
-      end
-      
-      notice_users = user.notice_points.sort_by{ rand }
-      np_count_all = notice_users.size
-      np_groups = []
-      if np_count_all <= 5
-        np_groups << notice_users
-      else
-        cut_point = (( np_count_all / 4 - 3 ) + np_count_all % 4) * 4
-        last_point = np_count_all - 1
-        three_groups = notice_users.slice!(cut_point..last_point)
-        notice_users.each_slice(4){ |four_users_group| np_groups << four_users_group }
-        three_groups.each_slice(3){ |three_users_group| np_groups << three_users_group }
-      end
-      
-      np_groups.each do |np_group|
-        members = np_group.map{ |member| member.name }.join(",")
-        np_group.each do |np|
-          Notifier.notice_email(np,shop,members)
-          Notifier.deliver_notice_email(np,shop,members)
-          logger.info "[Mail] send email to #{np.email}"
+        if rests.instance_of?(Array)
+          shop_count = rests.size
+          shop = rests[rand(shop_count)]
+        else
+          shop = rests
+        end
+
+        notice_users = user.notice_points.sort_by{ rand }
+        np_count_all = notice_users.size
+        np_groups = []
+        if np_count_all <= 5
+          np_groups << notice_users
+        elsif np_count_all == 8
+          notice_users.each_slice(4){ |four_users_group| np_groups << four_users_group }
+        else
+          cut_point = (( np_count_all / 4 - 3 ) + np_count_all % 4) * 4
+          last_point = np_count_all - 1
+          three_groups = notice_users.slice!(cut_point..last_point)
+          notice_users.each_slice(4){ |four_users_group| np_groups << four_users_group }
+          three_groups.each_slice(3){ |three_users_group| np_groups << three_users_group }
+        end
+
+        np_groups.each do |np_group|
+          members = np_group.map{ |member| member.name }.join(",")
+          np_group.each do |np|
+            Notifier.notice_email(np,shop,members)
+            Notifier.deliver_notice_email(np,shop,members)
+            logger.info "[Mail] send email to #{np.email}"
+          end
         end
       end
     end
