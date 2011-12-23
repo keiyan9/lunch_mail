f25c25386a93e773fcc0483d1bd8a55b2b3b080a
keiyan9 <nanbakouichi@nanba-no-MacBook-Air.local>
2011-12-23 21:55:54 +0900

    新規登録時のバグを修正した

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,14 @@
 class HomeController < ApplicationController
   skip_before_filter :authenticate_user!
   def index
-    return redirect_to setting_path(current_user.setting) if user_signed_in?
+    if user_signed_in?
+      if current_user.setting
+        return redirect_to setting_path(current_user.setting)
+      else
+        return redirect_to new_setting_path
+      end
+    else
+      return unless user_signed_in?
+    end
   end
 end
