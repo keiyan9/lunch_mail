35356b48e1e430e1a7c70cb6481966e6f476c31b
keiyan9 <nanbakouichi@nanba-no-MacBook-Air.local>
2012-02-29 21:14:11 +0900

    add column to users_table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120229120629) do
+ActiveRecord::Schema.define(:version => 20120229121157) do
 
   create_table "groups", :force => true do |t|
     t.string   "name"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(:version => 20120229120629) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "group_id"
+    t.boolean  "active"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
