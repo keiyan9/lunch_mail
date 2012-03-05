ef69a4271689be70cf8763b7ae9cee2bdf782efb
keiyan9 <nanbakouichi@nanba-no-MacBook-Air.local>
2012-03-05 19:05:02 +0900

    Fix design a bit

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,9 +3,9 @@ module UsersHelper
   def notice_td_content(user)
     content_tag(:div, :id => "notice-user-#{user.id}") do
       if user.active == true
-        link_to "通知中", notice_group_user_path(@group, user, :status => "false"), :remote => true, :class => "btn small primary"
+        link_to "通知中", notice_group_user_path(@group, user, :status => "false"), :remote => true, :class => "btn mini primary"
       else
-        link_to "通知する", notice_group_user_path(@group, user, :status => "true"), :remote => true, :class => "btn small"
+        link_to "通知する", notice_group_user_path(@group, user, :status => "true"), :remote => true, :class => "btn mini"
       end
     end
   end
